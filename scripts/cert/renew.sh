139271035eff7ccd4c975685b8e6f4d49f41e447
Tom Martensen <tmartens@redhat.com>
2023-04-17 15:47:07 +0200

    create certs

--- scripts/cert/renew.sh
+++ scripts/cert/renew.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir -p example
 openssl genrsa -out example/server.key 4096
 openssl req -nodes -new -x509 -sha256 -days 365 -config scripts/cert/localhost.cnf -extensions 'req_ext' -key example/server.key -out example/server.crt
