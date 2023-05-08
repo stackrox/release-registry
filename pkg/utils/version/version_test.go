aac815ad43c2dbd858ada89d5b4626ea29c2347e
Tom Martensen <tmartens@redhat.com>
2023-05-08 13:39:51 +0200

    fix

--- pkg/utils/version/version_test.go
+++ pkg/utils/version/version_test.go
@@ -11,7 +11,7 @@ func TestLatestDev(t *testing.T) {
 	assert.Equal(t, version.GetKind("1.2.x-24-gabcdef1234"), version.DevelopmentKind)
 	latest, err := version.LatestVersion([]string{"1.2.x-24-gabcdef1234", "1.2.x-123-gabcdef1234"})
 	assert.NoError(t, err)
-	assert.Equal(t, latest, "1.2.3-123-gabcdef1234")
+	assert.Equal(t, latest, "1.2.x-123-gabcdef1234")
 }
 
 func TestLatestReleaseVersion(t *testing.T) {
